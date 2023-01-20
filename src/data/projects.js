8aea6f123634f26f53a7b8717121f34f7e5e9ce7
Ingleson <jovemtksstranger@gmail.com>
2023-01-20 01:37:46 -0400

    access by link to code or project

--- src/data/projects.js
+++ src/data/projects.js
@@ -9,31 +9,36 @@ const projectList = [
     img: kenziehub,
     title: 'KenzieHub',
     description: 'Aplicação para anotações de tecnologias já aprendidas.',
-    link: 'https://kenzie-hub-vert-ten.vercel.app'
+    linkProject: 'https://kenzie-hub-vert-ten.vercel.app',
+    linkGithub: 'https://github.com/Ingleson/KenzieHub',
   },
   {
     img: miniquiz,
     title: 'Dev-Quiz',
     description: 'Pequeno quiz que testa o conhecimento básico do desenvolvedor.',
-    link: 'https://mini-quiz-lilac.vercel.app'
+    linkProject: 'https://mini-quiz-lilac.vercel.app',
+    linkGithub: 'https://github.com/Ingleson/mini-quiz'
   },
   {
     img: transactionApp,
     title: 'TransactionApp',
     description: 'Aplicação que faz uma simulação de antecipação.',
-    link: 'https://transaction-app-five.vercel.app'
+    linkProject: 'https://transaction-app-five.vercel.app',
+    linkGithub: 'https://github.com/Ingleson/TransactionApp'
   },
   {
     img: hamburguer,
     title: "Snack Shop",
     description: "Aplicação para compra de lanches e acompanhamentos.",
-    link: 'https://hamburgueria-eta-three.vercel.app/'
+    linkProject: 'https://hamburgueria-eta-three.vercel.app/',
+    linkGithub: 'https://github.com/Ingleson/Hamburgueria'
   },
   {
     img: nuKenzie,
     title: 'NuKenzie',
     description: 'Aplicação para gerenciamento de despesas.',
-    link: 'https://react-entrega-s1-nu-kenzie-ingleson.vercel.app/'
+    linkProject: 'https://react-entrega-s1-nu-kenzie-ingleson.vercel.app/',
+    linkGithub: 'https://github.com/Ingleson/NuKenzie'
   }
 ]
 
